f8f8c1bdeb74bf94589095f7da05a832ed586d4c
chengyi <0524eric@gmail.com>
2022-06-15 11:28:26 +0800

    商品功能 -實作修改商品

--- src/main/java/com/example/springbootmall/service/ProductService.java
+++ src/main/java/com/example/springbootmall/service/ProductService.java
@@ -6,6 +6,7 @@ import com.example.springbootmall.model.Product;
 public interface ProductService {
 
     Product getProductById(Integer productId);
-
     Integer createProduct(ProductRequest productRequest);
+
+    void updateProduct(Integer productId, ProductRequest productRequest);
 }
