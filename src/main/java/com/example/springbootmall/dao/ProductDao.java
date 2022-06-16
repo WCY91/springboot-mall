e0185fd54d9ff65d1e0b4f12848950827d87d061
chengyi <0524eric@gmail.com>
2022-06-16 16:25:18 +0800

    商品功能 -實作查詢商品列表

--- src/main/java/com/example/springbootmall/dao/ProductDao.java
+++ src/main/java/com/example/springbootmall/dao/ProductDao.java
@@ -3,7 +3,10 @@ package com.example.springbootmall.dao;
 import com.example.springbootmall.dto.ProductRequest;
 import com.example.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
--- src/main/java/com/example/springbootmall/service/ProductService.java
+++ src/main/java/com/example/springbootmall/service/ProductService.java
@@ -3,8 +3,10 @@ package com.example.springbootmall.service;
 import com.example.springbootmall.dto.ProductRequest;
 import com.example.springbootmall.model.Product;
 
-public interface ProductService {
+import java.util.List;
 
+public interface ProductService {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
